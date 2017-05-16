abbef0dba9ddbf2fccc8619505200215e6dc1515
Anthony Hortin <anthony@maddisondesigns.com>
2017-05-16 20:27:58 +1000

    Tweak Simple Notice and Single Accordion to only display limited HTML tags

--- inc/custom-controls.php
+++ inc/custom-controls.php
@@ -163,6 +163,7 @@ if ( class_exists( 'WP_Customize_Control' ) ) {
  					'href' => array(),
  					'title' => array(),
  					'class' => array(),
+					'target' => array(),
  				),
  				'br' => array(),
  				'em' => array(),
@@ -208,18 +209,32 @@ if ( class_exists( 'WP_Customize_Control' ) ) {
 		/**
 		 * Render the control in the customizer
 		 */
-		public function render_content() {
-		?>
-			<div class="simple-notice-custom-control">
-				<?php if( !empty( $this->label ) ) { ?>
-					<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
-				<?php } ?>
-				<?php if( !empty( $this->description ) ) { ?>
-					<span class="customize-control-description"><?php echo wp_kses_post( $this->description ); ?></span>
-				<?php } ?>
-			</div>
-		<?php
-		}
+		 public function render_content() {
+ 			$allowed_html = array(
+ 				'a' => array(
+ 					'href' => array(),
+ 					'title' => array(),
+ 					'class' => array(),
+ 					'target' => array(),
+ 				),
+ 				'br' => array(),
+ 				'em' => array(),
+ 				'strong' => array(),
+ 				'i' => array(
+ 					'class' => array()
+ 				),
+ 			);
+ 		?>
+ 			<div class="simple-notice-custom-control">
+ 				<?php if( !empty( $this->label ) ) { ?>
+ 					<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
+ 				<?php } ?>
+ 				<?php if( !empty( $this->description ) ) { ?>
+ 					<span class="customize-control-description"><?php echo wp_kses( $this->description, $allowed_html ); ?></span>
+ 				<?php } ?>
+ 			</div>
+ 		<?php
+ 		}
 	}
 
 	/**
