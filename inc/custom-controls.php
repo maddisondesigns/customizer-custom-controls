494ddc025a5ccf6b5ce1a3acec3159236f99136f
Anthony Hortin <hello@maddisondesigns.com>
2019-03-15 19:36:54 +1100

    Tweaked spacing as per WP coding standards

--- inc/custom-controls.php
+++ inc/custom-controls.php
@@ -14,9 +14,9 @@ if ( class_exists( 'WP_Customize_Control' ) ) {
 	 */
 	class Skyrocket_Custom_Control extends WP_Customize_Control {
 		protected function get_skyrocket_resource_url() {
-			if(strpos(wp_normalize_path(__DIR__), wp_normalize_path(WP_PLUGIN_DIR)) === 0){
+			if( strpos( wp_normalize_path( __DIR__ ), wp_normalize_path( WP_PLUGIN_DIR ) ) === 0 ) {
 				// We're in a plugin directory and need to determine the url accordingly.
-				return plugin_dir_url(__DIR__);
+				return plugin_dir_url( __DIR__ );
 			}
 
 			return trailingslashit( get_template_directory_uri() );
