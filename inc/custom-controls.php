7ca116a3a3c14ee83a2cd64d9fa4033faab0c4e6
Anthony Hortin <anthony@maddisondesigns.com>
2019-11-13 02:40:33 +1100

    Fixed issue with Sortable Pill Checkbox Control not displaying choices in correct saved order upon initial render
    Refer Issue #15

--- inc/custom-controls.php
+++ inc/custom-controls.php
@@ -940,7 +940,7 @@ if ( class_exists( 'WP_Customize_Control' ) ) {
 	 * @license http://www.gnu.org/licenses/gpl-2.0.html
 	 * @link https://github.com/maddisondesigns
 	 */
-	 class Skyrocket_Pill_Checkbox_Custom_Control extends Skyrocket_Custom_Control {
+	class Skyrocket_Pill_Checkbox_Custom_Control extends Skyrocket_Custom_Control {
 		/**
 		 * The type of control being rendered
 		 */
@@ -970,12 +970,28 @@ if ( class_exists( 'WP_Customize_Control' ) ) {
 		 * Enqueue our scripts and styles
 		 */
 		public function enqueue() {
+			wp_enqueue_script( 'skyrocket-custom-controls-js', $this->get_skyrocket_resource_url() . 'js/customizer.js', array( 'jquery' ), '1.0', true );
 			wp_enqueue_style( 'skyrocket-custom-controls-css', $this->get_skyrocket_resource_url() . 'css/customizer.css', array(), '1.0', 'all' );
 		}
 		/**
 		 * Render the control in the customizer
 		 */
 		public function render_content() {
+			$reordered_choices = array();
+			$saved_choices = explode( ',', esc_attr( $this->value() ) );
+
+			// Order the checkbox choices based on the saved order
+			if( $this->sortable ) {
+				foreach ( $saved_choices as $key => $value ) {
+					if( isset( $this->choices[$value] ) ) {
+						$reordered_choices[$value] = $this->choices[$value];
+					}
+				}
+				$reordered_choices = array_merge( $reordered_choices, array_diff_assoc( $this->choices, $reordered_choices ) );
+			}
+			else {
+				$reordered_choices = $this->choices;
+			}
 		?>
 			<div class="pill_checkbox_control">
 				<?php if( !empty( $this->label ) ) { ?>
@@ -984,12 +1000,11 @@ if ( class_exists( 'WP_Customize_Control' ) ) {
 				<?php if( !empty( $this->description ) ) { ?>
 					<span class="customize-control-description"><?php echo esc_html( $this->description ); ?></span>
 				<?php } ?>
-				<?php $chkboxValues = explode( ',', esc_attr( $this->value() ) ); ?>
 				<input type="hidden" id="<?php echo esc_attr( $this->id ); ?>" name="<?php echo esc_attr( $this->id ); ?>" value="<?php echo esc_attr( $this->value() ); ?>" class="customize-control-sortable-pill-checkbox" <?php $this->link(); ?> />
 				<div class="sortable_pills<?php echo ( $this->sortable ? ' sortable' : '' ) . ( $this->fullwidth ? ' fullwidth_pills' : '' ); ?>">
-				<?php foreach ( $this->choices as $key => $value ) { ?>
+				<?php foreach ( $reordered_choices as $key => $value ) { ?>
 					<label class="checkbox-label">
-						<input type="checkbox" name="<?php echo esc_attr( $key ); ?>" value="<?php echo esc_attr( $key ); ?>" <?php checked( in_array( esc_attr( $key ), $chkboxValues ), 1 ); ?> class="sortable-pill-checkbox"/>
+						<input type="checkbox" name="<?php echo esc_attr( $key ); ?>" value="<?php echo esc_attr( $key ); ?>" <?php checked( in_array( esc_attr( $key ), $saved_choices, true ), true ); ?> class="sortable-pill-checkbox"/>
 						<span class="sortable-pill-title"><?php echo esc_attr( $value ); ?></span>
 						<?php if( $this->sortable && $this->fullwidth ) { ?>
 							<span class="dashicons dashicons-sort"></span>
