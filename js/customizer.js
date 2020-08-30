c89a36ac0ac07d8e721c934145321c3d0b9f5834
Anthony Hortin <anthony@maddisondesigns.com>
2020-08-30 20:57:14 +1000

    Manually initialise WPColorPicker Alpha Color control so I can include color palettes

--- js/customizer.js
+++ js/customizer.js
@@ -369,6 +369,23 @@ jQuery( document ).ready(function($) {
 	});
 
 	/**
+	 * WP ColorPicker Alpha Color Picker Control
+	 *
+	 * @author Anthony Hortin <http://maddisondesigns.com>
+	 * @license http://www.gnu.org/licenses/gpl-2.0.html
+	 * @link https://github.com/maddisondesigns
+	 */
+
+	// Manually initialise the wpColorPicker controls so we can add the color picker palette
+	$('.wpcolorpicker-alpha-color-picker').each(function( i, obj ) {
+		var paletteColors = _wpCustomizeSettings.controls[$(this).attr('id')].colorpickerpalette;
+		var options = {
+			palettes: paletteColors
+		};
+		$(obj).wpColorPicker( options );
+	} );
+
+	/**
  	 * Alpha Color Picker Custom Control
  	 *
  	 * @author Braad Martin <http://braadmartin.com>
