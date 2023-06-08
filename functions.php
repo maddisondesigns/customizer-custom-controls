fcc650bd6581d0b02a22846ef8b6a486293868ce
Anthony Hortin <anthony@maddisondesigns.com>
2023-06-08 20:13:27 +1000

    Updated FontAwesome version. Added new social icons

--- functions.php
+++ functions.php
@@ -10,8 +10,8 @@
 if ( ! function_exists( 'skyrocket_scripts_styles' ) ) {
 	function skyrocket_scripts_styles() {
 		// Register and enqueue our icon font
-		// We're using the awesome Font Awesome icon font. http://fortawesome.github.io/Font-Awesome
-		wp_register_style( 'fontawesome', trailingslashit( get_template_directory_uri() ) . 'css/fontawesome-all.min.css' , array(), '5.8.2', 'all' );
+		// We're using the awesome Font Awesome icon font. https://fontawesome.com
+		wp_register_style( 'fontawesome', trailingslashit( get_template_directory_uri() ) . 'css/fontawesome-all.min.css' , array(), '6.4.0', 'all' );
 		wp_enqueue_style( 'fontawesome' );
 	}
 }
@@ -51,39 +51,52 @@ function skyrocket_is_woocommerce_active() {
  */
 if ( ! function_exists( 'skyrocket_generate_social_urls' ) ) {
 	function skyrocket_generate_social_urls() {
+		$plurality = apply_filters( 'skyrocket_social_icons_plurality', _x( 'me', 'First-person pronoun for Social Icon titles', 'skyrocket' ) );
+
 		$social_icons = array(
-			array( 'url' => 'behance.net', 'icon' => 'fab fa-behance', 'title' => esc_html__( 'Follow me on Behance', 'skyrocket' ), 'class' => 'behance' ),
-			array( 'url' => 'bitbucket.org', 'icon' => 'fab fa-bitbucket', 'title' => esc_html__( 'Fork me on Bitbucket', 'skyrocket' ), 'class' => 'bitbucket' ),
-			array( 'url' => 'codepen.io', 'icon' => 'fab fa-codepen', 'title' => esc_html__( 'Follow me on CodePen', 'skyrocket' ), 'class' => 'codepen' ),
-			array( 'url' => 'deviantart.com', 'icon' => 'fab fa-deviantart', 'title' => esc_html__( 'Watch me on DeviantArt', 'skyrocket' ), 'class' => 'deviantart' ),
-			array( 'url' => 'discord.gg', 'icon' => 'fab fa-discord', 'title' => esc_html__( 'Join me on Discord', 'skyrocket' ), 'class' => 'discord' ),
-			array( 'url' => 'dribbble.com', 'icon' => 'fab fa-dribbble', 'title' => esc_html__( 'Follow me on Dribbble', 'skyrocket' ), 'class' => 'dribbble' ),
-			array( 'url' => 'etsy.com', 'icon' => 'fab fa-etsy', 'title' => esc_html__( 'favorite me on Etsy', 'skyrocket' ), 'class' => 'etsy' ),
-			array( 'url' => 'facebook.com', 'icon' => 'fab fa-facebook-f', 'title' => esc_html__( 'Like me on Facebook', 'skyrocket' ), 'class' => 'facebook' ),
-			array( 'url' => 'flickr.com', 'icon' => 'fab fa-flickr', 'title' => esc_html__( 'Connect with me on Flickr', 'skyrocket' ), 'class' => 'flickr' ),
-			array( 'url' => 'foursquare.com', 'icon' => 'fab fa-foursquare', 'title' => esc_html__( 'Follow me on Foursquare', 'skyrocket' ), 'class' => 'foursquare' ),
-			array( 'url' => 'github.com', 'icon' => 'fab fa-github', 'title' => esc_html__( 'Fork me on GitHub', 'skyrocket' ), 'class' => 'github' ),
-			array( 'url' => 'instagram.com', 'icon' => 'fab fa-instagram', 'title' => esc_html__( 'Follow me on Instagram', 'skyrocket' ), 'class' => 'instagram' ),
-			array( 'url' => 'kickstarter.com', 'icon' => 'fab fa-kickstarter-k', 'title' => esc_html__( 'Back me on Kickstarter', 'skyrocket' ), 'class' => 'kickstarter' ),
-			array( 'url' => 'last.fm', 'icon' => 'fab fa-lastfm', 'title' => esc_html__( 'Follow me on Last.fm', 'skyrocket' ), 'class' => 'lastfm' ),
-			array( 'url' => 'linkedin.com', 'icon' => 'fab fa-linkedin-in', 'title' => esc_html__( 'Connect with me on LinkedIn', 'skyrocket' ), 'class' => 'linkedin' ),
-			array( 'url' => 'medium.com', 'icon' => 'fab fa-medium-m', 'title' => esc_html__( 'Follow me on Medium', 'skyrocket' ), 'class' => 'medium' ),
-			array( 'url' => 'patreon.com', 'icon' => 'fab fa-patreon', 'title' => esc_html__( 'Support me on Patreon', 'skyrocket' ), 'class' => 'patreon' ),
-			array( 'url' => 'pinterest.com', 'icon' => 'fab fa-pinterest-p', 'title' => esc_html__( 'Follow me on Pinterest', 'skyrocket' ), 'class' => 'pinterest' ),
-			array( 'url' => 'plus.google.com', 'icon' => 'fab fa-google-plus-g', 'title' => esc_html__( 'Connect with me on Google+', 'skyrocket' ), 'class' => 'googleplus' ),
-			array( 'url' => 'reddit.com', 'icon' => 'fab fa-reddit-alien', 'title' => esc_html__( 'Join me on Reddit', 'skyrocket' ), 'class' => 'reddit' ),
-			array( 'url' => 'slack.com', 'icon' => 'fab fa-slack-hash', 'title' => esc_html__( 'Join me on Slack', 'skyrocket' ), 'class' => 'slack.' ),
-			array( 'url' => 'slideshare.net', 'icon' => 'fab fa-slideshare', 'title' => esc_html__( 'Follow me on SlideShare', 'skyrocket' ), 'class' => 'slideshare' ),
-			array( 'url' => 'snapchat.com', 'icon' => 'fab fa-snapchat-ghost', 'title' => esc_html__( 'Add me on Snapchat', 'skyrocket' ), 'class' => 'snapchat' ),
-			array( 'url' => 'soundcloud.com', 'icon' => 'fab fa-soundcloud', 'title' => esc_html__( 'Follow me on SoundCloud', 'skyrocket' ), 'class' => 'soundcloud' ),
-			array( 'url' => 'spotify.com', 'icon' => 'fab fa-spotify', 'title' => esc_html__( 'Follow me on Spotify', 'skyrocket' ), 'class' => 'spotify' ),
-			array( 'url' => 'stackoverflow.com', 'icon' => 'fab fa-stack-overflow', 'title' => esc_html__( 'Join me on Stack Overflow', 'skyrocket' ), 'class' => 'stackoverflow' ),
-			array( 'url' => 'tumblr.com', 'icon' => 'fab fa-tumblr', 'title' => esc_html__( 'Follow me on Tumblr', 'skyrocket' ), 'class' => 'tumblr' ),
-			array( 'url' => 'twitch.tv', 'icon' => 'fab fa-twitch', 'title' => esc_html__( 'Follow me on Twitch', 'skyrocket' ), 'class' => 'twitch' ),
-			array( 'url' => 'twitter.com', 'icon' => 'fab fa-twitter', 'title' => esc_html__( 'Follow me on Twitter', 'skyrocket' ), 'class' => 'twitter' ),
-			array( 'url' => 'vimeo.com', 'icon' => 'fab fa-vimeo-v', 'title' => esc_html__( 'Follow me on Vimeo', 'skyrocket' ), 'class' => 'vimeo' ),
-			array( 'url' => 'weibo.com', 'icon' => 'fab fa-weibo', 'title' => esc_html__( 'Follow me on weibo', 'skyrocket' ), 'class' => 'weibo' ),
-			array( 'url' => 'youtube.com', 'icon' => 'fab fa-youtube', 'title' => esc_html__( 'Subscribe to me on YouTube', 'skyrocket' ), 'class' => 'youtube' ),
+			array( 'url' => '500px.com', 'icon' => 'fab fa-500px', 'title' => esc_html( sprintf( __( 'Follow %s on 500px', 'skyrocket' ), $plurality ) ), 'class' => 'fivehundredpx' ),
+			array( 'url' => 'artstation.com', 'icon' => 'fab fa-artstation', 'title' => esc_html( sprintf( __( 'Follow %s on ArtStation', 'skyrocket' ), $plurality ) ), 'class' => 'artstation' ),
+			array( 'url' => 'behance.net', 'icon' => 'fab fa-behance', 'title' => esc_html( sprintf( __( 'Follow %s on Behance', 'skyrocket' ), $plurality ) ), 'class' => 'behance' ),
+			array( 'url' => 'bitbucket.org', 'icon' => 'fab fa-bitbucket', 'title' => esc_html( sprintf( __( 'Fork %s on Bitbucket', 'skyrocket' ), $plurality ) ), 'class' => 'bitbucket' ),
+			array( 'url' => 'codepen.io', 'icon' => 'fab fa-codepen', 'title' => esc_html( sprintf( __( 'Follow %s on CodePen', 'skyrocket' ), $plurality ) ), 'class' => 'codepen' ),
+			array( 'url' => 'deviantart.com', 'icon' => 'fab fa-deviantart', 'title' => esc_html( sprintf( __( 'Watch %s on DeviantArt', 'skyrocket' ), $plurality ) ), 'class' => 'deviantart' ),
+			array( 'url' => 'discord.gg', 'icon' => 'fab fa-discord', 'title' => esc_html( sprintf( __( 'Join %s on Discord', 'skyrocket' ), $plurality ) ), 'class' => 'discord' ),
+			array( 'url' => 'dribbble.com', 'icon' => 'fab fa-dribbble', 'title' => esc_html( sprintf( __( 'Follow %s on Dribbble', 'skyrocket' ), $plurality ) ), 'class' => 'dribbble' ),
+			array( 'url' => 'etsy.com', 'icon' => 'fab fa-etsy', 'title' => esc_html( sprintf( __( 'favorite %s on Etsy', 'skyrocket' ), $plurality ) ), 'class' => 'etsy' ),
+			array( 'url' => 'facebook.com', 'icon' => 'fab fa-facebook-f', 'title' => esc_html( sprintf( __( 'Like %s on Facebook', 'skyrocket' ), $plurality ) ), 'class' => 'facebook' ),
+			array( 'url' => 'figma.com', 'icon' => 'fab fa-figma', 'title' => esc_html( sprintf( __( 'Follow %s on Figma', 'skyrocket' ), $plurality ) ), 'class' => 'figma' ),
+			array( 'url' => 'flickr.com', 'icon' => 'fab fa-flickr', 'title' => esc_html( sprintf( __( 'Connect with %s on Flickr', 'skyrocket' ), $plurality ) ), 'class' => 'flickr' ),
+			array( 'url' => 'foursquare.com', 'icon' => 'fab fa-foursquare', 'title' => esc_html( sprintf( __( 'Follow %s on Foursquare', 'skyrocket' ), $plurality ) ), 'class' => 'foursquare' ),
+			array( 'url' => 'github.com', 'icon' => 'fab fa-github', 'title' => esc_html( sprintf( __( 'Fork %s on GitHub', 'skyrocket' ), $plurality ) ), 'class' => 'github' ),
+			array( 'url' => 'instagram.com', 'icon' => 'fab fa-instagram', 'title' => esc_html( sprintf( __( 'Follow %s on Instagram', 'skyrocket' ), $plurality ) ), 'class' => 'instagram' ),
+			array( 'url' => 'kickstarter.com', 'icon' => 'fab fa-kickstarter-k', 'title' => esc_html( sprintf( __( 'Back %s on Kickstarter', 'skyrocket' ), $plurality ) ), 'class' => 'kickstarter' ),
+			array( 'url' => 'last.fm', 'icon' => 'fab fa-lastfm', 'title' => esc_html( sprintf( __( 'Follow %s on Last.fm', 'skyrocket' ), $plurality ) ), 'class' => 'lastfm' ),
+			array( 'url' => 'linkedin.com', 'icon' => 'fab fa-linkedin-in', 'title' => esc_html( sprintf( __( 'Connect with %s on LinkedIn', 'skyrocket' ), $plurality ) ), 'class' => 'linkedin' ),
+			array( 'url' => 'mastodon.social', 'icon' => 'fab fa-mastodon', 'title' => esc_html( sprintf( __( 'Follow %s on Mastodon', 'skyrocket' ), $plurality ) ), 'class' => 'mastodon' ),
+			array( 'url' => 'mastodon.art', 'icon' => 'fab fa-mastodon', 'title' => esc_html( sprintf( __( 'Follow %s on Mastodon', 'skyrocket' ), $plurality ) ), 'class' => 'mastodon' ),
+			array( 'url' => 'medium.com', 'icon' => 'fab fa-medium-m', 'title' => esc_html( sprintf( __( 'Follow %s on Medium', 'skyrocket' ), $plurality ) ), 'class' => 'medium' ),
+			array( 'url' => 'patreon.com', 'icon' => 'fab fa-patreon', 'title' => esc_html( sprintf( __( 'Support %s on Patreon', 'skyrocket' ), $plurality ) ), 'class' => 'patreon' ),
+			array( 'url' => 'pinterest.com', 'icon' => 'fab fa-pinterest-p', 'title' => esc_html( sprintf( __( 'Follow %s on Pinterest', 'skyrocket' ), $plurality ) ), 'class' => 'pinterest' ),
+			array( 'url' => 'quora.com', 'icon' => 'fab fa-quora', 'title' => esc_html( sprintf( __( 'Follow %s on Quora', 'skyrocket' ), $plurality ) ), 'class' => 'Quora' ),
+			array( 'url' => 'reddit.com', 'icon' => 'fab fa-reddit-alien', 'title' => esc_html( sprintf( __( 'Join %s on Reddit', 'skyrocket' ), $plurality ) ), 'class' => 'reddit' ),
+			array( 'url' => 'slack.com', 'icon' => 'fab fa-slack-hash', 'title' => esc_html( sprintf( __( 'Join %s on Slack', 'skyrocket' ), $plurality ) ), 'class' => 'slack.' ),
+			array( 'url' => 'slideshare.net', 'icon' => 'fab fa-slideshare', 'title' => esc_html( sprintf( __( 'Follow %s on SlideShare', 'skyrocket' ), $plurality ) ), 'class' => 'slideshare' ),
+			array( 'url' => 'snapchat.com', 'icon' => 'fab fa-snapchat-ghost', 'title' => esc_html( sprintf( __( 'Add %s on Snapchat', 'skyrocket' ), $plurality ) ), 'class' => 'snapchat' ),
+			array( 'url' => 'soundcloud.com', 'icon' => 'fab fa-soundcloud', 'title' => esc_html( sprintf( __( 'Follow %s on SoundCloud', 'skyrocket' ), $plurality ) ), 'class' => 'soundcloud' ),
+			array( 'url' => 'spotify.com', 'icon' => 'fab fa-spotify', 'title' => esc_html( sprintf( __( 'Follow %s on Spotify', 'skyrocket' ), $plurality ) ), 'class' => 'spotify' ),
+			array( 'url' => 'stackoverflow.com', 'icon' => 'fab fa-stack-overflow', 'title' => esc_html( sprintf( __( 'Join %s on Stack Overflow', 'skyrocket' ), $plurality ) ), 'class' => 'stackoverflow' ),
+			array( 'url' => 'steamcommunity.com', 'icon' => 'fab fa-steam', 'title' => esc_html( sprintf( __( 'Follow %s on Steam', 'skyrocket' ), $plurality ) ), 'class' => 'steam' ),
+			array( 'url' => 't.me', 'icon' => 'fab fa-telegram', 'title' => esc_html( sprintf( __( 'Chat with %s on Telegram', 'skyrocket' ), $plurality ) ), 'class' => 'Telegram' ),
+			array( 'url' => 'tiktok.com', 'icon' => 'fab fa-tiktok', 'title' => esc_html( sprintf( __( 'Follow %s on TikTok', 'skyrocket' ), $plurality ) ), 'class' => 'tiktok' ),
+			array( 'url' => 'tumblr.com', 'icon' => 'fab fa-tumblr', 'title' => esc_html( sprintf( __( 'Follow %s on Tumblr', 'skyrocket' ), $plurality ) ), 'class' => 'tumblr' ),
+			array( 'url' => 'twitch.tv', 'icon' => 'fab fa-twitch', 'title' => esc_html( sprintf( __( 'Follow %s on Twitch', 'skyrocket' ), $plurality ) ), 'class' => 'twitch' ),
+			array( 'url' => 'twitter.com', 'icon' => 'fab fa-twitter', 'title' => esc_html( sprintf( __( 'Follow %s on Twitter', 'skyrocket' ), $plurality ) ), 'class' => 'twitter' ),
+			array( 'url' => 'assetstore.unity.com', 'icon' => 'fab fa-unity', 'title' => esc_html( sprintf( __( 'Follow %s on Unity Asset Store', 'skyrocket' ), $plurality ) ), 'class' => 'unity' ),
+			array( 'url' => 'unsplash.com', 'icon' => 'fab fa-unsplash', 'title' => esc_html( sprintf( __( 'Follow %s on Unsplash', 'skyrocket' ), $plurality ) ), 'class' => 'unsplash' ),
+			array( 'url' => 'vimeo.com', 'icon' => 'fab fa-vimeo-v', 'title' => esc_html( sprintf( __( 'Follow %s on Vimeo', 'skyrocket' ), $plurality ) ), 'class' => 'vimeo' ),
+			array( 'url' => 'weibo.com', 'icon' => 'fab fa-weibo', 'title' => esc_html( sprintf( __( 'Follow %s on weibo', 'skyrocket' ), $plurality ) ), 'class' => 'weibo' ),
+			array( 'url' => 'wa.me', 'icon' => 'fab fa-whatsapp', 'title' => esc_html( sprintf( __( 'Chat with %s on WhatsApp', 'skyrocket' ), $plurality ) ), 'class' => 'WhatsApp' ),
+			array( 'url' => 'youtube.com', 'icon' => 'fab fa-youtube', 'title' => esc_html( sprintf( __( 'Subscribe to %s on YouTube', 'skyrocket' ), $plurality ) ), 'class' => 'youtube' ),
 		);
 
 		return apply_filters( 'skyrocket_social_icons', $social_icons );
